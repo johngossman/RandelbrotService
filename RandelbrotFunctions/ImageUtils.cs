bd4e343281ff0264a2b15c4266c8ada63d208625
John Gossman <johngos@microsoft.com>
2017-03-31 12:29:23 -0700

    Fixed color  space issues

--- RandelbrotFunctions/ImageUtils.cs
+++ RandelbrotFunctions/ImageUtils.cs
@@ -11,23 +11,44 @@ namespace RandelbrotFunctions
     {
         public byte[] PixelBufferToJPEG(PixelBuffer buffer)
         {
-            int stride = buffer.SizeX * sizeof(int);
-            byte[] bytes = new byte[stride];
-            Buffer.BlockCopy(buffer.GetPixels(), 0, bytes, 0, bytes.Length);
+            // RGB -- 3 components per pixel
+            const int components = 3;
+
+            byte[] rgbBits = this.bufferToRGB(buffer);
+
+            int stride = buffer.SizeX * components; 
+            byte[] rowBits = new byte[stride];
 
             SampleRow[] rows = new SampleRow[buffer.SizeY];
             for(int i = 0; i < buffer.SizeY; i++)
             {
-                Buffer.BlockCopy(buffer.GetPixels(), i * stride, bytes, 0, stride);
-                rows[i] = new SampleRow(bytes, buffer.SizeX, 8, 4);
+                Buffer.BlockCopy(rgbBits, i * stride, rowBits, 0, stride);
+                rows[i] = new SampleRow(rowBits, buffer.SizeX, 8, components);
             }
 
-            JpegImage image = new JpegImage(rows, Colorspace.CMYK);
+            JpegImage image = new JpegImage(rows, Colorspace.RGB);
 
             MemoryStream stream = new MemoryStream();
             image.WriteJpeg(stream);
 
             return stream.GetBuffer();
+        }
+
+        private byte[] bufferToRGB(PixelBuffer buffer)
+        {
+            // RGB == 3 components per pixel
+            byte[] rgbBits = new byte[buffer.SizeX * buffer.SizeY * 3];
+
+            int[] pixels = buffer.GetPixels(); // In ARGB format
+            for(int i = 0; i < pixels.Length; i++)
+            {
+                int offset = i * 3;
+                rgbBits[offset] = (byte)(byte)((pixels[i] >> 16) & 0xff);   // red
+                rgbBits[offset + 1] = (byte)((pixels[i] >> 8) & 0xff);  // green
+                rgbBits[offset + 2] = (byte)(pixels[i] & 0xff); // blue
+            }
+
+            return rgbBits;
         }
     }
 }
